8628906ab72d45916b8a66d7af77459080f3f8ec
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2025-02-22 20:14:12 +0100

    Add TypeORM and configuration support for PostgreSQL database. Next - Custom configuration files

--- database-01/src/app.module.ts
+++ database-01/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.prod', '.env.development', '.env.local', '.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_DATABASE || 'test',
+      entities: [],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
